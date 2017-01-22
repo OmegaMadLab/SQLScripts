c4f39439b2c03eb43a4e3c2d9d3f41bb041dc4db
Unknown <locurtola@tin.it>
2017-01-22 16:52:42 +0100

    FinalPackageReorg

---
+++ tcdit2017/sqlvm-alwayson-cluster/WIP/postDeploymentScript/environmentSetup.ps1
@@ -0,0 +1,305 @@
+﻿$mainDatacenterSubnet = '10.0.0.0/16'
+$drDatacenterSubnet = '172.16.0.0/16'
+
+$mainADDC = 'mo3-addc-main'
+$drADDC = 'mo3-addc-dr'
+
+$ilbIpMainSubnet = '10.0.1.9'
+$ilbIpDrSubnet = '172.16.1.9'
+
+$primaryReplicaName = 'mo3-sql-main0'
+$secondaryReplicaName = 'mo3-sql-main1'
+$drReplicaName = 'mo3-sql-dr0'
+$domainFqdn = 'tcdit17demo.local'
+
+$clusterName = 'sqlhademo'
+$fswPath = '\\mo3-fsw-main\cluster-fsw'
+$mainClusterNetName = 'Main datacenter network'
+$drClusterNetName = 'DR datacenter network'
+$mainClusterIpName = 'Main datacenter cluster IP'
+$drClusterIpName = 'DR datacenter cluster IP'
+
+$sqlNodes = @()
+$sqlNodes += $primaryReplicaName
+$sqlNodes += $secondaryReplicaName
+$sqlNodes += $drReplicaName
+
+$sqlAgent = 'SQLSERVERAGENT'
+$sqlSrv = 'MSSQLSERVER'
+$backupShare = "F:\Backup"
+$sqlSvcAcctn = "tcdit17demo\sjSqlAdmin"
+$db = 'TCDIT17DEMO'
+$ag = 'AlwaysOn-AG'
+$agListener = 'aglistener'
+
+Import-Module FailoverClusters
+Import-Module ActiveDirectory
+pushd
+Import-Module sqlPs -DisableNameChecking
+popd
+
+
+if($primaryReplicaName -ne $env:computername) {
+    Write-Host "Please execute this script on "$primaryReplica
+    Return
+}
+
+
+
+#==========================================================================
+#        Create AD Topology
+#==========================================================================
+
+
+#Rename default-first-site-name
+pushd
+cd 'ad:CN=Sites,CN=Configuration*'
+ren cn=def* cn=MainDatacenter
+popd
+
+#Add subnet to main AD site
+$mainADSite = Get-ADReplicationSite
+New-ADReplicationSubnet -Name $mainDatacenterSubnet -Site $mainADSite -Server $mainADDC
+
+#Add dr AD site with appropriate subnet
+New-ADReplicationSite -Name 'DrDatacenter' -Server $mainADDC
+$drADSite = Get-ADReplicationSite -Identity 'DrDatacenter' -Server $mainADDC
+New-ADReplicationSubnet -Name $drDatacenterSubnet -Site $drADSite -Server $mainADDC
+
+#Create new site link and remove the default one
+New-ADReplicationSiteLink -Name Main2DR -Cost 100 -InterSiteTransportProtocol IP -ReplicationFrequencyInMinutes 15 -SitesIncluded $mainADSite,$drADSite
+Get-ADReplicationSiteLink -Identity DEFAULTIPSITELINK | Remove-ADReplicationSiteLink -Confirm:$false
+
+#Move DR ADDC to appropriate site
+Move-ADDirectoryServer -Identity $drADDC -Site $drADSite
+
+#Enable intersite Change Notification - Be carefull on production environment!
+$siteLink = Get-ADReplicationSiteLink -Identity Main2DR
+Get-adobject -Identity $siteLink.DistinguishedName -properties options | set-adobject –replace @{options=$($_.options –bor 1)} 
+
+#Reduce DNS -> AD polling interval - Be carefull on production environment!
+invoke-command -Computername $mainADDC, $drADDC -scriptBlock{set-DnsServerDsSetting -PollingInterval 30|Restart-Service -Force dns}
+
+#==========================================================================
+#        Create multisubnet WSFC
+#==========================================================================
+
+#Create multisubnet WSFC
+New-Cluster -Name $clusterName -Node $sqlNodes
+$cluster = Get-Cluster
+
+$clusterGroup = $cluster | Get-ClusterGroup
+$clusterNameRes = $clusterGroup | Get-ClusterResource "Cluster Name"
+$clusterNameRes | Stop-ClusterResource | Out-Null
+
+#Rename cluster networks and change IPs to Link Local IP addresses in order to avoid errors due to Azure DHCP, and bring cluster network name online
+$clusterNetworks = Get-ClusterNetwork
+
+ForEach($clusterNetwork in $clusterNetworks) {
+   
+    if($clusterNetwork.Address.contains($mainDatacenterSubnet.Substring(0,$mainDatacenterSubnet.IndexOf('.')))){
+        $clusterNetwork.Name = $mainClusterNetName
+    }
+    else {
+        $clusterNetwork.Name = $drClusterNetName
+    }
+}
+
+$clusterIpAddrRes = $clusterGroup | Get-ClusterResource | Where-Object { $_.ResourceType.Name -eq "IP Address"}          
+
+ForEach($clusterIpAddr in $clusterIpAddrRes) {
+    
+    $clusterIpAddr | Stop-ClusterResource | Out-Null
+    
+    if(($clusterIpAddr | Get-ClusterParameter -Name 'Network').Value -eq $mainClusterNetName) {
+        $clusterIpAddr.Name = $mainClusterIpName
+        $IpAddr = '169.254.1.10'
+    }
+    else {
+        $clusterIpAddr.Name = $drClusterIpName
+        $IpAddr = '169.254.10.10'
+    }
+
+    $clusterIpAddr | Set-ClusterParameter -Multiple @{
+        "Address" = "$ipAddr"
+        "SubnetMask" = "255.255.0.0"
+        "EnableDhcp" = 0
+        "OverrideAddressMatch" = 1
+    } -ErrorAction Stop
+}
+
+Start-ClusterResource $clusterNameRes
+
+#Change cluster quorum configuration to node and file share majority
+Set-ClusterQuorum -FileShareWitness $fswPath
+
+#Remove vote from dr node to avoid unwanted failover due to remote site connectivity issues
+(Get-ClusterNode -Name $drReplica).NodeWeight = 0
+
+
+#==========================================================================
+#        Configure SQL AlwaysOn AG
+#==========================================================================
+
+#Enable TCP protocol and HADR feature on all instances
+$smo = 'Microsoft.SqlServer.Management.Smo.'
+$timeout = New-Object System.TimeSpan -ArgumentList 0, 0, 30
+Add-Type -AssemblyName System.ServiceProcess
+
+ForEach ($sqlNode in $sqlNodes) {
+    $wmi = new-object ($smo + 'Wmi.ManagedComputer')$sqlNode
+    $uri = "ManagedComputer[@Name='" + $sqlNode + "']/ ServerInstance[@Name='MSSQLSERVER']/ServerProtocol[@Name='Tcp']"
+    $Tcp = $wmi.GetSmoObject($uri)
+    $Tcp.IsEnabled = $true
+    $Tcp.Alter()
+
+    Enable-SqlAlwaysOn `
+     -Path SQLSERVER:\SQL\$sqlNode\Default `
+     -NoServiceRestart
+
+    $sqlSvc = get-service -ComputerName $sqlNode -Name $sqlSrv
+    if($sqlSvc.Status -eq "Running") {
+        $sqlSvc.Stop()
+    }
+    $sqlSvc.WaitForStatus("Stopped", $timeout)
+    $sqlSvc.Start()
+    $sqlSvc.WaitForStatus("Running", $timeout)
+}
+
+#Create backup share
+New-Item $backupShare -ItemType directory
+net share backup=$backupShare "/grant:$sqlSvcAcctn,FULL"
+icacls.exe "$backupShare" /grant:r ("$sqlSvcAcctn" + ":(OI)(CI)F")
+
+#Create database on every instance
+Invoke-SqlCmd -Query "CREATE database $db"
+Backup-SqlDatabase -Database $db -BackupFile "$backupShare\$db.bak" -ServerInstance $primaryReplicaName
+Backup-SqlDatabase -Database $db -BackupFile "$backupShare\$db.log" -ServerInstance $primaryReplicaName -BackupAction Log
+Restore-SqlDatabase -Database $db -BackupFile "\\$primaryReplicaName\backup\$db.bak" -ServerInstance $secondaryReplicaName -NoRecovery
+Restore-SqlDatabase -Database $db -BackupFile "\\$primaryReplicaName\backup\$db.log" -ServerInstance $secondaryReplicaName -RestoreAction Log -NoRecovery
+Restore-SqlDatabase -Database $db -BackupFile "\\$primaryReplicaName\backup\$db.bak" -ServerInstance $drReplicaName -NoRecovery
+Restore-SqlDatabase -Database $db -BackupFile "\\$primaryReplicaName\backup\$db.log" -ServerInstance $drReplicaName -RestoreAction Log -NoRecovery
+
+#Mirroring endpoint creation
+ForEach ($sqlNode in $sqlNodes) {
+    $endpoint =
+        New-SqlHadrEndpoint SqlHaEndpoint `
+        -Port 5022 `
+        -Path "SQLSERVER:\SQL\$sqlNode\Default"
+    Set-SqlHadrEndpoint `
+        -InputObject $endpoint `
+        -State "Started"
+    Invoke-SqlCmd -Query "CREATE LOGIN [$sqlSvcAcctn] FROM WINDOWS" -ServerInstance $sqlNode
+    Invoke-SqlCmd -Query "GRANT CONNECT ON ENDPOINT::[SqlHaEndpoint] TO [$sqlSvcAcctn]" -ServerInstance $sqlNode
+}
+
+#Availability Replicas creation
+$primaryReplica =
+    New-SqlAvailabilityReplica `
+    -Name $primaryReplicaName `
+    -EndpointURL "TCP://$primaryReplicaName.$domainFqdn`:5022" `
+    -AvailabilityMode "SynchronousCommit" `
+    -FailoverMode "Automatic" `
+    -Version 11 `
+    -AsTemplate
+$secondaryReplica =
+    New-SqlAvailabilityReplica `
+    -Name $secondaryReplicaName `
+    -EndpointURL "TCP://$secondaryReplicaName.$domainFqdn`:5022" `
+    -AvailabilityMode "SynchronousCommit" `
+    -FailoverMode "Automatic" `
+    -Version 11 `
+    -AsTemplate
+$drReplica =
+    New-SqlAvailabilityReplica `
+    -Name $drReplicaName `
+    -EndpointURL "TCP://$drReplicaName.$domainFqdn`:5022" `
+    -AvailabilityMode "AsynchronousCommit" `
+    -FailoverMode "Manual" `
+    -Version 11 `
+    -AsTemplate
+
+#AVG Creation
+New-SqlAvailabilityGroup `
+     -Name $ag `
+     -Path "SQLSERVER:\SQL\$primaryReplicaName\Default" `
+     -AvailabilityReplica @($primaryReplica,$secondaryReplica,$drReplica) `
+     -Database $db
+Join-SqlAvailabilityGroup `
+    -Path "SQLSERVER:\SQL\$secondaryReplicaName\Default" `
+    -Name $ag
+Add-SqlAvailabilityDatabase `
+    -Path "SQLSERVER:\SQL\$secondaryReplicaName\Default\AvailabilityGroups\$ag" `
+    -Database $db
+Join-SqlAvailabilityGroup `
+    -Path "SQLSERVER:\SQL\$drReplicaName\Default" `
+    -Name $ag
+Add-SqlAvailabilityDatabase `
+    -Path "SQLSERVER:\SQL\$drReplicaName\Default\AvailabilityGroups\$ag" `
+    -Database $db
+
+#ReadOnly Replicas and routing list
+$primaryReplica = Get-Item "SQLSERVER:\SQL\$primaryReplicaName\Default\availabilityGroups\$ag\availabilityReplicas\$primaryReplicaName"
+$secondaryReplica = Get-Item "SQLSERVER:\SQL\$primaryReplicaName\Default\availabilityGroups\$ag\availabilityReplicas\$secondaryReplicaName"
+$drReplica = Get-Item "SQLSERVER:\SQL\$primaryReplicaName\Default\availabilityGroups\$ag\availabilityReplicas\$drReplicaName"
+
+Set-SqlAvailabilityReplica -ConnectionModeInPrimaryRole "AllowAllConnections" -InputObject $primaryReplica
+Set-SqlAvailabilityReplica -ConnectionModeInSecondaryRole "AllowReadIntentConnectionsOnly" -InputObject $primaryReplica
+Set-SqlAvailabilityReplica -ReadOnlyRoutingConnectionUrl "TCP://$primaryReplicaName.$domainFqdn`:1433" -InputObject $primaryReplica
+
+Set-SqlAvailabilityReplica -ConnectionModeInPrimaryRole "AllowAllConnections" -InputObject $secondaryReplica
+Set-SqlAvailabilityReplica -ConnectionModeInSecondaryRole "AllowReadIntentConnectionsOnly" -InputObject $secondaryReplica
+Set-SqlAvailabilityReplica -ReadOnlyRoutingConnectionUrl "TCP://$secondaryReplicaName.$domainFqdn`:1433" -InputObject $secondaryReplica
+
+Set-SqlAvailabilityReplica -ConnectionModeInPrimaryRole "AllowAllConnections" -InputObject $drReplica
+Set-SqlAvailabilityReplica -ConnectionModeInSecondaryRole "AllowReadIntentConnectionsOnly" -InputObject $drReplica
+Set-SqlAvailabilityReplica -ReadOnlyRoutingConnectionUrl "TCP://$drReplicaName.$domainFqdn`:1433" -InputObject $drReplica
+
+Set-SqlAvailabilityReplica -ReadOnlyRoutingList "$secondaryReplicaName","$drReplicaName","$primaryReplicaName" -InputObject $primaryReplica
+Set-SqlAvailabilityReplica -ReadOnlyRoutingList "$primaryReplicaName","$drReplicaName","$secondaryReplicaName" -InputObject $secondaryReplica
+Set-SqlAvailabilityReplica -ReadOnlyRoutingList "$primaryReplicaName","$secondaryReplicaName","$drReplicaName" -InputObject $drReplica
+
+
+#==========================================================================
+#        Configure SQL AlwaysOn AG Listener
+#==========================================================================
+
+#Client Access Point creation
+$agClusterGroup = Get-ClusterGroup -Name $ag
+#1. Add a network name named as the AG Listener
+$cap = Add-ClusterResource -Name $agListener -ResourceType "Network Name" -Group $agClusterGroup
+#2. Set paramters
+$cap | Set-ClusterParameter -Multiple @{
+    "DnsName" = "$agListener"
+    "HostRecordTTL" = 30
+    "RegisterAllProvidersIP" = 1
+} -ErrorAction Stop
+#3. Create two IP Addresses, assigning them the Azure ILB ip created on each vnet
+$capIpMain = Add-ClusterResource -Name "ILB IP main datacenter" -ResourceType "IP Address" -Group $agClusterGroup
+$capIpMain | Set-ClusterParameter -Multiple @{
+        "Address" = "$ilbIpMainSubnet"
+        "SubnetMask" = "255.255.255.255"
+        "Network" = "$mainClusterNetName"
+        "EnableDHCP" = 0
+        "ProbePort" = "59999"
+    } -ErrorAction Stop
+
+$capIpDr = Add-ClusterResource -Name "ILB IP dr datacenter" -ResourceType "IP Address" -Group $agClusterGroup
+$capIpDr | Set-ClusterParameter -Multiple @{
+        "Address" = "$ilbIpDrSubnet"
+        "SubnetMask" = "255.255.255.255"
+        "Network" = "$drClusterNetName"
+        "EnableDHCP" = 0
+        "ProbePort" = "59999"
+    } -ErrorAction Stop
+#4. Add dependencies on IPs to Network Name
+Set-ClusterResourceDependency -InputObject $cap -Dependency "[$capIpMain] or [$capIpDr]"
+#5. Add AG resource dependency on CAP network name
+$agResource = Get-ClusterResource -Name $ag 
+$agResource | Stop-ClusterResource
+Add-ClusterResourceDependency -InputObject $agResource -Resource $cap
+$cap|Start-ClusterResource
+$agResource | Start-ClusterResource
+#6. Assign port 1433 to SQL AG Listener
+Set-SqlAvailabilityGroupListener -Port 1433
+
---
+++ tcdit2017/sqlvm-alwayson-cluster/scripts/PrepareSqlServerVM.ps1/PrepareSqlServerVM.ps1
@@ -0,0 +1,352 @@
+#
+# Copyright="© Microsoft Corporation. All rights reserved."
+#
+
+configuration PrepareSqlServerVM
+{
+    param
+    (
+        [Parameter(Mandatory)]
+        [String]$DomainName,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$Admincreds,
+
+        [Parameter(Mandatory)]
+        [System.Management.Automation.PSCredential]$SQLServicecreds,
+
+        [System.Management.Automation.PSCredential]$SQLAuthCreds,
+
+        [UInt32]$DatabaseEnginePort = 1433,
+
+        [Parameter(Mandatory)]
+        [UInt32]$NumberOfDisks,
+
+        [Parameter(Mandatory)]
+        [String]$WorkloadType,
+
+        [String]$DomainNetbiosName=(Get-NetBIOSName -DomainName $DomainName),
+
+        [Parameter(Mandatory)]
+        [String]$ClusterName,
+
+        [Parameter(Mandatory)]
+        [String]$SharePath,
+
+        [Parameter(Mandatory)]
+        [String[]]$Nodes,
+
+        [Int]$RetryCount=20,
+        [Int]$RetryIntervalSec=30
+    )
+
+    Import-DscResource -ModuleName xComputerManagement, xFailOverCluster,CDisk,xActiveDirectory,xDisk,xSqlPs,xNetworking, xSql, xSQLServer
+    
+    [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($Admincreds.UserName)", $Admincreds.Password)
+    [System.Management.Automation.PSCredential]$DomainFQDNCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
+    [System.Management.Automation.PSCredential]$SQLCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($SQLServicecreds.UserName)", $SQLServicecreds.Password)
+
+    Enable-CredSSPNTLM -DomainName $DomainName
+
+    $RebootVirtualMachine = $false
+
+    if ($DomainName)
+    {
+        $RebootVirtualMachine = $true
+    }
+
+    WaitForSqlSetup
+
+    Node localhost
+    {
+        xSqlCreateVirtualDisk CreateVirtualDisk
+        {
+            DriveSize = $NumberOfDisks
+            NumberOfColumns = $NumberOfDisks
+            BytesPerDisk = 1099511627776
+            OptimizationType = $WorkloadType
+            RebootVirtualMachine = $RebootVirtualMachine
+        }
+
+        WindowsFeature FC
+        {
+            Name = "Failover-Clustering"
+            Ensure = "Present"
+        }
+
+        WindowsFeature FailoverClusterTools 
+        { 
+            Ensure = "Present" 
+            Name = "RSAT-Clustering-Mgmt"
+            DependsOn = "[WindowsFeature]FC"
+        } 
+
+        WindowsFeature FCPS
+        {
+            Name = "RSAT-Clustering-PowerShell"
+            Ensure = "Present"
+        }
+
+        WindowsFeature ADPS
+        {
+            Name = "RSAT-AD-PowerShell"
+            Ensure = "Present"
+        }
+
+        xWaitForADDomain DscForestWait 
+        { 
+            DomainName = $DomainName 
+            DomainUserCredential= $DomainCreds
+            RetryCount = $RetryCount 
+            RetryIntervalSec = $RetryIntervalSec 
+	        DependsOn = "[WindowsFeature]ADPS"
+        }
+        
+        xComputer DomainJoin
+        {
+            Name = $env:COMPUTERNAME
+            DomainName = $DomainName
+            Credential = $DomainCreds
+	        DependsOn = "[xWaitForADDomain]DscForestWait"
+        }
+
+        xFirewall DatabaseEngineFirewallRule
+        {
+            Direction = "Inbound"
+            Name = "SQL-Server-Database-Engine-TCP-In"
+            DisplayName = "SQL Server Database Engine (TCP-In)"
+            Description = "Inbound rule for SQL Server to allow TCP traffic for the Database Engine."
+            DisplayGroup = "SQL Server"
+            State = "Enabled"
+            Access = "Allow"
+            Protocol = "TCP"
+            LocalPort = $DatabaseEnginePort -as [String]
+            Ensure = "Present"
+        }
+
+        xFirewall DatabaseMirroringFirewallRule
+        {
+            Direction = "Inbound"
+            Name = "SQL-Server-Database-Mirroring-TCP-In"
+            DisplayName = "SQL Server Database Mirroring (TCP-In)"
+            Description = "Inbound rule for SQL Server to allow TCP traffic for the Database Mirroring."
+            DisplayGroup = "SQL Server"
+            State = "Enabled"
+            Access = "Allow"
+            Protocol = "TCP"
+            LocalPort = "5022"
+            Ensure = "Present"
+        }
+
+        xFirewall ListenerFirewallRule
+        {
+            Direction = "Inbound"
+            Name = "SQL-Server-Availability-Group-Listener-TCP-In"
+            DisplayName = "SQL Server Availability Group Listener (TCP-In)"
+            Description = "Inbound rule for SQL Server to allow TCP traffic for the Availability Group listener."
+            DisplayGroup = "SQL Server"
+            State = "Enabled"
+            Access = "Allow"
+            Protocol = "TCP"
+            LocalPort = "59999"
+            Ensure = "Present"
+        }
+
+        LocalConfigurationManager 
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        xCluster FailoverCluster
+        {
+            Name = $ClusterName
+            DomainAdministratorCredential = $DomainCreds
+            Nodes = $Nodes
+        }
+
+        xWaitForFileShareWitness WaitForFSW
+        {
+            SharePath = $SharePath
+            DomainAdministratorCredential = $DomainCreds
+            DependsOn = "[xCluster]FailoverCluster"
+
+        }
+
+        xClusterQuorum FailoverClusterQuorum
+        {
+            Name = $ClusterName
+            SharePath = $SharePath
+            DomainAdministratorCredential = $DomainCreds
+        }
+
+        LocalConfigurationManager 
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        xSqlLogin AddDomainAdminAccountToSysadminServerRole
+        {
+            Name = $DomainCreds.UserName
+            LoginType = "WindowsUser"
+            ServerRoles = "sysadmin"
+            Enabled = $true
+            Credential = $Admincreds
+            DependsOn = "[xCluster]FailoverCluster"
+        }
+
+        xADUser CreateSqlServerServiceAccount
+        {
+            DomainAdministratorCredential = $DomainCreds
+            DomainName = $DomainName
+            UserName = $SQLServicecreds.UserName
+            Password = $SQLServicecreds
+            Ensure = "Present"
+            DependsOn = "[xSqlLogin]AddDomainAdminAccountToSysadminServerRole"
+        }
+
+        xSqlLogin AddSqlServerServiceAccountToSysadminServerRole
+        {
+            Name = $SQLCreds.UserName
+            LoginType = "WindowsUser"
+            ServerRoles = "sysadmin"
+            Enabled = $true
+            Credential = $Admincreds
+            DependsOn = "[xADUser]CreateSqlServerServiceAccount"
+        }
+        
+        xSqlTsqlEndpoint AddSqlServerEndpoint
+        {
+            InstanceName = "MSSQLSERVER"
+            PortNumber = $DatabaseEnginePort
+            SqlAdministratorCredential = $Admincreds
+            DependsOn = "[xSqlLogin]AddSqlServerServiceAccountToSysadminServerRole"
+        }
+
+        xSQLServerStorageSettings AddSQLServerStorageSettings
+        {
+            InstanceName = "MSSQLSERVER"
+            OptimizationType = $WorkloadType
+            DependsOn = "[xSqlTsqlEndpoint]AddSqlServerEndpoint"
+        }
+
+        xSqlServer ConfigureSqlServerWithAlwaysOn
+        {
+            InstanceName = $env:COMPUTERNAME
+            SqlAdministratorCredential = $Admincreds
+            ServiceCredential = $SQLCreds
+            MaxDegreeOfParallelism = 1
+            FilePath = "F:\DATA"
+            LogPath = "F:\LOG"
+            Hadr = "Enabled"
+            DomainAdministratorCredential = $DomainFQDNCreds
+            DependsOn = "[xSqlLogin]AddSqlServerServiceAccountToSysadminServerRole"
+        }
+
+    }
+}
+function Get-NetBIOSName
+{ 
+    [OutputType([string])]
+    param(
+        [string]$DomainName
+    )
+
+    if ($DomainName.Contains('.')) {
+        $length=$DomainName.IndexOf('.')
+        if ( $length -ge 16) {
+            $length=15
+        }
+        return $DomainName.Substring(0,$length)
+    }
+    else {
+        if ($DomainName.Length -gt 15) {
+            return $DomainName.Substring(0,15)
+        }
+        else {
+            return $DomainName
+        }
+    }
+}
+function WaitForSqlSetup
+{
+    # Wait for SQL Server Setup to finish before proceeding.
+    while ($true)
+    {
+        try
+        {
+            Get-ScheduledTaskInfo "\ConfigureSqlImageTasks\RunConfigureImage" -ErrorAction Stop
+            Start-Sleep -Seconds 5
+        }
+        catch
+        {
+            break
+        }
+    }
+}
+function Update-DNS
+{
+    param(
+        [string]$LBName,
+        [string]$LBAddress,
+        [string]$DomainName
+
+        )
+               
+        $ARecord=Get-DnsServerResourceRecord -Name $LBName -ZoneName $DomainName -ErrorAction SilentlyContinue -RRType A
+        if (-not $Arecord)
+        {
+            Add-DnsServerResourceRecordA -Name $LBName -ZoneName $DomainName -IPv4Address $LBAddress
+        }
+}
+function Enable-CredSSPNTLM
+{ 
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$DomainName
+    )
+    
+    # This is needed for the case where NTLM authentication is used
+
+    Write-Verbose 'STARTED:Setting up CredSSP for NTLM'
+   
+    Enable-WSManCredSSP -Role client -DelegateComputer localhost, *.$DomainName -Force -ErrorAction SilentlyContinue
+    Enable-WSManCredSSP -Role server -Force -ErrorAction SilentlyContinue
+
+    if(-not (Test-Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -ErrorAction SilentlyContinue))
+    {
+        New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows -Name '\CredentialsDelegation' -ErrorAction SilentlyContinue
+    }
+
+    if( -not (Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name 'AllowFreshCredentialsWhenNTLMOnly' -ErrorAction SilentlyContinue))
+    {
+        New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name 'AllowFreshCredentialsWhenNTLMOnly' -value '1' -PropertyType dword -ErrorAction SilentlyContinue
+    }
+
+    if (-not (Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name 'ConcatenateDefaults_AllowFreshNTLMOnly' -ErrorAction SilentlyContinue))
+    {
+        New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name 'ConcatenateDefaults_AllowFreshNTLMOnly' -value '1' -PropertyType dword -ErrorAction SilentlyContinue
+    }
+
+    if(-not (Test-Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -ErrorAction SilentlyContinue))
+    {
+        New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation -Name 'AllowFreshCredentialsWhenNTLMOnly' -ErrorAction SilentlyContinue
+    }
+
+    if (-not (Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name '1' -ErrorAction SilentlyContinue))
+    {
+        New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name '1' -value "wsman/$env:COMPUTERNAME" -PropertyType string -ErrorAction SilentlyContinue
+    }
+
+    if (-not (Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name '2' -ErrorAction SilentlyContinue))
+    {
+        New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name '2' -value "wsman/localhost" -PropertyType string -ErrorAction SilentlyContinue
+    }
+
+    if (-not (Get-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name '3' -ErrorAction SilentlyContinue))
+    {
+        New-ItemProperty HKLM:\SOFTWARE\Policies\Microsoft\Windows\CredentialsDelegation\AllowFreshCredentialsWhenNTLMOnly -Name '3' -value "wsman/*.$DomainName" -PropertyType string -ErrorAction SilentlyContinue
+    }
+
+    Write-Verbose "DONE:Setting up CredSSP for NTLM"
+}
+
