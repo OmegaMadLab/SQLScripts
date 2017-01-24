d2b4012c801d1e3930ce0847364a6c79865feceb
Unknown <locurtola@tin.it>
2017-01-24 22:06:54 +0100

    sdas

--- tcdit2017/sqlvm-alwayson-cluster/scripts/PrepareAlwaysOnSqlServer.ps1/PrepareAlwaysOnSqlServer.ps1
+++ tcdit2017/sqlvm-alwayson-cluster/scripts/PrepareAlwaysOnSqlServer.ps1/PrepareAlwaysOnSqlServer.ps1
@@ -31,7 +31,7 @@ configuration PrepareAlwaysOnSqlServer
         [Int]$RetryIntervalSec=30
     )
 
-    Import-DscResource -ModuleName xComputerManagement,CDisk,xActiveDirectory,XDisk,xSql,xSQLServer, xSQLps,xNetworking
+    Import-DscResource -ModuleName xComputerManagement,CDisk,xActiveDirectory,XDisk,xSql, xSQLServer, xSQLps,xNetworking
     [System.Management.Automation.PSCredential]$DomainCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($Admincreds.UserName)", $Admincreds.Password)
     [System.Management.Automation.PSCredential]$DomainFQDNCreds = New-Object System.Management.Automation.PSCredential ("${DomainName}\$($Admincreds.UserName)", $Admincreds.Password)
     [System.Management.Automation.PSCredential]$SQLCreds = New-Object System.Management.Automation.PSCredential ("${DomainNetbiosName}\$($SQLServicecreds.UserName)", $SQLServicecreds.Password)
